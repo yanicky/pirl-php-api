4680a060d5903d003ed8a82493dbd5c5bed1ad71
yanicky <33847588+yanicky@users.noreply.github.com>
2019-07-29 10:57:07 -0400

    Create test-api.sh
    to accelerate testing

---
+++ test-api.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Testing without parameters";
+php index.php;
+echo "***";
+echo "Testing with bad wallet parameters";
+php index.php --wallet=0x256b2b26Fe8eCAd201103946F8C603b401cE16E;
+echo "***";
+echo "Testing with good wallet parameters";
+php index.php --wallet=0x256b2b26Fe8eCAd201103946F8C603b401cE16EC;
+echo "***";
+echo "Testing getBalance with Ethereum parameter"; 
+php index.php --wallet=0x256b2b26Fe8eCAd201103946F8C603b401cE16EC --chain=Ethereum;
+echo "***";
+echo "Testing getBalance with bad chain parameter";
+php index.php --wallet=0x256b2b26Fe8eCAd201103946F8C603b401cE16EC --chain=Ethereu;
+echo "***";
+echo "Testing blockNumber with Ethereum parameter";
+php index.php --chain=Pirl --CMD=blockNumber;
+echo "***";
+echo "Testing blockNumber with Ethereum parameter";
+php index.php --chain=Ethereum --CMD=blockNumber;
+echo "***";
