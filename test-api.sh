97ddab01b6d2318645d55231aed7224e0ae274bc
yanicky <33847588+yanicky@users.noreply.github.com>
2019-07-31 19:24:25 -0400

    Update test-api.sh
    added --id= to some test

--- test-api.sh
+++ test-api.sh
@@ -15,26 +15,26 @@ echo "Testing getBalance with bad chain parameter";
 php index.php --wallet=0x256b2b26Fe8eCAd201103946F8C603b401cE16EC --chain=Ethereu;
 echo "***";
 echo "Testing blockNumber with Pirl chain";
-php index.php --chain=Pirl --CMD=blockNumber;
+php index.php --chain=Pirl --CMD=blockNumber --id=1;
 echo "***";
 echo "Testing blockNumber with Ethereum chain";
-php index.php --chain=Ethereum --CMD=blockNumber;
+php index.php --chain=Ethereum --CMD=blockNumber --id=2;
 echo "***";
 echo "Testing peerCount with default chain";
-php index.php --CMD=peerCount;
+php index.php --CMD=peerCount --id=3;
 echo "***";
 echo "Testing peerCount with Ethereum chain";
-php index.php --CMD=peerCount --chain=Ethereum;
+php index.php --CMD=peerCount --chain=Ethereum --id=4;
 echo "***";
 echo "Testing net_version with default chain";
-php index.php --CMD=net_version --chain=Pirl
+php index.php --CMD=net_version --chain=Pirl --id=5
 echo "***";
 echo "Testing net_version with Ethereum chain";
-php index.php --CMD=net_version --chain=Ethereum
+php index.php --CMD=net_version --chain=Ethereum --id=6
 echo "***";
 echo "Testing net_version with Default chain";
-php index.php --CMD=web3_clientVersion
+php index.php --CMD=web3_clientVersion --id=7
 echo "***";
 echo "Testing net_version with Ethereum chain";
-php index.php --CMD=web3_clientVersion --chain=Ethereum
+php index.php --CMD=web3_clientVersion --chain=Ethereum --id=8
 echo "***";
